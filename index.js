0a8edcb9ca905054fbc6b9414ea9304a6de599c1
Lucas Di Benedetto <102391726+LucasDiBs@users.noreply.github.com>
2023-09-17 14:54:51 -0300

    Add files via upload
    Todo el tp Front end hasta ahora

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+console.log("Cargo el DOM");
+const registrobtn = document.getElementById("registrarseBTN");
+
+console.log(registrobtn);
+registrobtn.addEventListener("click",()=>{
+
+
+console.log("Abarajame en la bañera nena");
+
+})
+});
+
+
+
